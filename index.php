179e659d6f381658000b8c01e0fae9db16d7dec2
sjeuquayroyal <sieunguyen22@gmail.com>
2021-11-07 17:46:41 +0700

    ok

--- index.php
+++ index.php
@@ -210,7 +210,7 @@ include_once("connection.php");
             <div class="row">
                 <div class="col-lg-3">
                     <div class="header__logo">
-                        <a href="?page=content"><img src="ATNtoy/Logo.jpg" alt=""></a>
+                        <a href="?page=content"><img src="ATNtoy/Logo.png" alt=""></a>
                     </div>
                 </div>
                 
@@ -381,7 +381,7 @@ include_once("connection.php");
                 <div class="col-lg-3 col-md-6 col-sm-6">
                     <div class="footer__about">
                         <div class="footer__about__logo">
-                            <a href="./index.html"><img src="ATNtoy/Logo.jpg" alt=""></a>
+                            <a href="./index.html"><img src="ATNtoy/Logo.png" alt=""></a>
                         </div>
                         <ul>
                             <li>Address: 160 Road 30/4 An Phu, Ninh Kieu, Can Tho, Vietnam</li>
