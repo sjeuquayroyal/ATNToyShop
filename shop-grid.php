3978465795e666970543afa1bad7f96aaca24467
sjeuquayroyal <sieunguyen22@gmail.com>
2021-11-08 11:09:58 +0700

    ok

--- shop-grid.php
+++ shop-grid.php
@@ -270,7 +270,7 @@
                                         <div class="product__discount__item__text">
                                             <span> </span>
                                             <h5><a href="?page=shop-details&&id=<?php echo  $row['product_id'] ?>"><?php echo $row["product_name"] ?></a></h5>
-                                            <div class="product__item__price"><?php echo $row["price"]  ?>đ </div>
+                                            <div class="product__item__price"><?php echo $row["price"]  ?>$</div>
                                         </div>
                                     </div>
                                 </div>
@@ -327,7 +327,7 @@
 
                         <div class="col-lg-4 col-md-6 col-sm-6">
                             <div class="product__item">
-                                <div class="product__item__pic set-bg" data-setbg="img/<?php echo $row['Pro_image'] ?>">
+                                <div class="product__item__pic set-bg" data-setbg="img/<?php echo $row['pro_image'] ?>">
                                     <ul class="product__item__pic__hover">
                                         <li><a href="#"><i class="fa fa-heart"></i></a></li>
                                         <li><a href="#"><i class="fa fa-retweet"></i></a></li>
@@ -336,7 +336,7 @@
                                 </div>
                                 <div class="product__item__text">
                                     <h6><a href="?page=shop-details&&id=<?php echo  $row['product_id'] ?>"><?php echo $row["product_name"] ?></a></h6>
-                                    <h5><?php echo $row["price"] ?>đ</h5>
+                                    <h5><?php echo $row["price"] ?>$</h5>
                                 </div>
                             </div>
                         </div>
