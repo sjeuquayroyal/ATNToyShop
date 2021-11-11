2cbf4b5c95fc4dc001001ce639dad6be83d295dd
sjeuquayroyal <sieunguyen22@gmail.com>
2021-11-11 14:15:49 +0700

    ok

--- Product_Management.php
+++ Product_Management.php
@@ -135,7 +135,7 @@
                                     from product, category where product.cat_id = category.cat_id and '$id'=category.cat_id ");
             
                                 }else{
-                                $result = pg_query($conn,"SELECT product.product_id, product.product_name, product.price, product.pro_qty, product.pro_image, category.cat_name 
+                                $result = pg_query($conn,"SELECT product.product_id, product.product_name, product.price, product.pro_qty, product.pro_image, category.cat_name, product.smalldesc
                                     from product, category where product.cat_id = category.cat_id ");
                                 }
                                 while($row=pg_fetch_array($result, NULL, PGSQL_ASSOC)) { 
