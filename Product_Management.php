8f402bc54a821a263f8c9f154a56c388ead98da7
sjeuquayroyal <sieunguyen22@gmail.com>
2021-11-12 13:26:28 +0700

    ok

--- Product_Management.php
+++ Product_Management.php
@@ -126,12 +126,12 @@
                             <?php //
                                  if(isset($_GET['id'])){
                                     $id=$_GET['id'];
-                                    $result = pg_query($conn,"SELECT product.product_id, product.product_name, /*ranch.branch_nam*/, product.price, product.pro_qty, product.pro_image, category.cat_name 
-                                    from product, category, branch where product.cat_id = category.cat_id and /*product.branch_id=branch.branch_id*/ '$id'=category.cat_id ");
+                                    $result = pg_query($conn,"SELECT product.product_id, product.product_name, ranch.branch_name, product.price, product.pro_qty, product.pro_image, category.cat_name 
+                                    from product, category, branch where product.cat_id = category.cat_id and product.branch_id=branch.branch_id '$id'=category.cat_id ");
             
                                 }else{
                                     $result = pg_query($conn,"SELECT product.product_id, product.product_name,branch.branch_name, product.price, product.pro_qty, product.pro_image, category.cat_name 
-                                    from product, category, branch where product.cat_id = category.cat_id /*and product.branch_id=branch.branch_id*/ ");
+                                    from product, category, branch where product.cat_id = category.cat_id and product.branch_id=branch.branch_id ");
                                 }
                                 while($row=pg_fetch_array($result, NULL, PGSQL_ASSOC)) { 
                                     ?>
