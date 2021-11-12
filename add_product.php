209c5672bec64181b81e8bdbda11c5696fe468d5
sjeuquayroyal <sieunguyen22@gmail.com>
2021-11-12 10:57:23 +0700

    okk

--- add_product.php
+++ add_product.php
@@ -191,7 +191,7 @@
 							</div>
 				</div>            
                 <div class="form-group">   
-                    <label for="lblDetail" class="col-sm-2 control-label">Detail Description(*):  </label>
+                    <label for="lblDetail" class="col-sm-2 control-label"><br>Detail Description(*):  </label>
 							<div class="col-sm-10">
 							      <textarea type="text" name="txtDetail" id="txtDetail" class="form-control" style="height: 150px" row="4" value=""></textarea>
 							</div>
