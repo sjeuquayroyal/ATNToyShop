0d917ce9cee7c2b33a9f5cd77459151f3d06b47f
sjeuquayroyal <sieunguyen22@gmail.com>
2021-11-12 14:00:55 +0700

    ok

--- branch.php
+++ branch.php
@@ -1,13 +1,78 @@
     <!-- Breadcrumb Section Begin -->
-    <section class="breadcrumb-section set-bg" data-setbg="ATNimg/one.jpg">
+    <script>
+        function deleteConfirm(){
+            if(confirm("Are you sure?")){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    </script>
+    
+   
+
+    <!-- Hero Section Begin -->
+    <section class="hero hero-normal">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="hero__categories">
+                        <div class="hero__categories__all">
+                            <i class="fa fa-bars"></i>
+                            <span>All Departments</span>
+                        <ul>
+                            <?php Department($conn); ?>
+                        </ul>
+                        </div>
+                        
+                        <ul>
+                        <li ><a  href="?page=pm">All</a></li>
+
+                        <?php Category_List($conn ); ?>
+                            
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-9">
+                    <div class="hero__search">
+                        <div class="hero__search__form">
+                            <form action="#">
+                                <div class="hero__search__categories">
+                                    All Categories
+                                    <span class="arrow_carrot-down"></span>
+                                    
+                                </div>
+                                <input type="text" placeholder="What do yo u need?">
+                                <button type="submit" class="site-btn">SEARCH</button>
+                            </form>
+                        </div>
+                        <div class="hero__search__phone">
+                            <div class="hero__search__phone__icon">
+                                <i class="fa fa-phone"></i>
+                            </div>
+                            <div class="hero__search__phone__text">
+                                <h5>+84 09 0785 3006</h5>
+                                <span>support 24/7 time</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Hero Section End -->
+
+    <!-- Breadcrumb Section Begin -->
+    <section class="breadcrumb-section set-bg" data-setbg="ATNtoy/background.jpg">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
                     <div class="breadcrumb__text">
                         <h2>Branch Management</h2>
                         <div class="breadcrumb__option">
-                            <a href="./index.html">Home</a>
-                            <span>Branch Management</span>
+                            <a href="?page=content">Home</a>
+                            <span>Branchs Management</span>
                         </div>
                     </div>
                 </div>
