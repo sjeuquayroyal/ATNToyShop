322f3c5b7fa38fd66e21dbc65093632acefd39c8
sjeuquayroyal <sieunguyen22@gmail.com>
2021-11-12 14:41:24 +0700

    ok

--- branch.php
+++ branch.php
@@ -1,45 +1,20 @@
-<span>All Departments</span>
-                        <ul>
-                            <?php Department($conn); ?>
-                        </ul>
-                        </div>
-                        
-                        <ul>
-                        <li ><a  href="?page=branch">All</a></li>
-
-                        <?php Branch_List($conn ); ?>
-                            
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-9">
-                    <div class="hero__search">
-                        <div class="hero__search__form">
-                            <form action="#">
-                                <div class="hero__search__categories">
-                                    All Branches
-                                    <span class="arrow_carrot-down"></span>
-                                    
-                                </div>
-                                <input type="text" placeholder="What do you need?">
-                                <button type="submit" class="site-btn">SEARCH</button>
-                            </form>
-                        </div>
-                        <div class="hero__search__phone">
-                            <div class="hero__search__phone__icon">
-                                <i class="fa fa-phone"></i>
-                            </div>
-                            <div class="hero__search__phone__text">
-                                <h5>+84 09 0785 3006</h5>
-                                <span>support 24/7 time</span>
-                            </div>
+    <!-- Breadcrumb Section Begin -->
+    <section class="breadcrumb-section set-bg" data-setbg="ATNtoy/background.jpg">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <div class="breadcrumb__text">
+                        <h2>Branch Management</h2>
+                        <div class="breadcrumb__option">
+                            <a href="./index.html">Home</a>
+                            <span>Branch Management</span>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </section>
-    <!-- Hero Section End -->
+    <!-- Breadcrumb Section End -->
 
     <!-- Breadcrumb Section Begin -->
     <section class="breadcrumb-section set-bg" data-setbg="ATNtoy/background.jpg">
