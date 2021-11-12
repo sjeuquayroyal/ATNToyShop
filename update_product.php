4939775bf0772f240a43693de756c4a1dbce0ad3
sjeuquayroyal <sieunguyen22@gmail.com>
2021-11-12 16:07:15 +0700

    ok

--- update_product.php
+++ update_product.php
@@ -1,3 +1,22 @@
+
+
+    <!-- Breadcrumb Section Begin -->
+    <section class="breadcrumb-section set-bg" data-setbg="ATNtoy/background.jpg">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <div class="breadcrumb__text">
+                        <h2>Products Management</h2>
+                        <div class="breadcrumb__option">
+                            <a href="./index.html">Home</a>
+                            <span>Products Management</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Breadcrumb Section End -->
 <!-- Hero Section Begin -->
 <section class="hero hero-normal">
         <div class="container">
