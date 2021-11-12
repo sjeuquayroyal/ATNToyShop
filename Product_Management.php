4b21f88404681e5649ac3b26a348832df6c2e703
sjeuquayroyal <sieunguyen22@gmail.com>
2021-11-12 12:50:09 +0700

    ok

--- Product_Management.php
+++ Product_Management.php
@@ -126,7 +126,7 @@
                             <?php //
                                  if(isset($_GET['id'])){
                                     $id=$_GET['id'];
-                                    $result = pg_query($conn,"SELECT product.product_id, product.product_name, product.price, product.pro_qty, product.pro_image, category.cat_name 
+                                    $result = pg_query($conn,"SELECT product.product_id, product.product_name, product.branch_id, product.price, product.pro_qty, product.pro_image, category.cat_name 
                                     from product, category where product.cat_id = category.cat_id and '$id'=category.cat_id ");
             
                                 }else{
